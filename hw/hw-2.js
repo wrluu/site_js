340ceaf3d8d278845f451a2171380303407a9ee2
wrluu <fikobookmy@gnail.com>
2025-06-03 20:18:32 +0300

    hm2

---
+++ hw/hw-2.js
@@ -0,0 +1,74 @@
+//task1
+//let a = 10;
+//alert(a)
+//a = 20;
+//alert(a)
+
+//task2
+//const date = 2007;
+//alert(date) 
+
+//task3
+//const namejs = 'Брендан Эйк';
+//alert(namejs)
+//task4
+
+//сложение
+//let a=10
+//let b=2
+//let result = a + b;
+//alert (result)
+
+//вычитание
+//let a=10 
+//let b=2
+//let result = a - b;
+//alert(result)
+
+//умножение 
+//let a=10 
+//let b=2
+//let result= a * b;
+//alert(result)
+
+//деление
+//let a=10
+//let b=2
+//let result= a/b;
+//alert(result)
+
+//task5
+//let a=2 
+//let result=(a**5)
+//alert(result)
+
+//task6
+//let a=9
+//let b=2
+//let result=(a%b)
+//alert(result)
+ 
+//task7 
+//let num = 1;
+//num += 5;
+//num -= 3;
+//num *= 7;
+//num /= 3;
+//num += 1;
+//num -= 1;
+//alert(num);
+
+//task8
+//let age= prompt('Cоколько вам лет?');
+//alert(age)
+
+//task9
+//const user = {
+    //name: 'Иван',
+    //age: 19,
+    //isAdmin:true,}
+
+//task10
+//let userName=prompt('Как вас зовут?') ;  
+//alert( `Привет, ${userName}!`)
+
